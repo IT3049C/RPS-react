d7c0dc3b6445c30b0bef6042ba43961edbf0a06e
Yahya Gilany <yahya.gilany@live.com>
2024-03-18 16:54:46 -0400

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/RPS-Game/Game.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import App from './routes/RPS-Game/Game.jsx'
+import ErrorPage from './routes/error.jsx'
+
+const routes = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <App />,
+    exact: true,
+    errorElement: <ErrorPage />,
+  },
+], {basename: '/rps-react'})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
